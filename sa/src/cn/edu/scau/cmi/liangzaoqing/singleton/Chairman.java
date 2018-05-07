852ecf5dea158968ab60103b5d65d20bfd4655a6
xt_lzq@126.com <xt_lzq@126.com>
2018-05-07 23:19:36 +0800

    添加单例模式。

--- sa/src/cn/edu/scau/cmi/liangzaoqing/singleton/Chairman.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/singleton/Chairman.java
@@ -2,4 +2,17 @@ package cn.edu.scau.cmi.liangzaoqing.singleton;
 
 public class Chairman {
 
+	private static Chairman chairman;
+
+	private Chairman() {
+	}
+
+	public static Chairman getInstance() {
+		if (chairman == null) {
+			chairman=new Chairman();
+			return chairman;
+		} else
+			return chairman;
+
+	}
 }
