320ace314f7997e44b56ea37b61a795bda44fb0c
xt_lzq@126.com <xt_lzq@126.com>
2018-05-08 16:12:57 +0800

    refactor

--- sa/src/cn/edu/scau/cmi/liangzaoqing/client/SingletonClient.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/SingletonClient.java
@@ -6,14 +6,14 @@ public class SingletonClient {
 
 	public static void main(String[] args) {
 
-		Chairman chairman1=Chairman.getInstance();
+		Chairman chairman1=Chairman.getInstance("毛泽东");
 		
 		
 		
 		
 		
 		
-		Chairman chairman2=Chairman.getInstance();
+		Chairman chairman2=Chairman.getInstance("梁早清");
 		
 		if(chairman1==chairman2){
 			System.out.println("一样的");
@@ -25,11 +25,11 @@ public class SingletonClient {
 		
 		
 		
-		System.out.println(Chairman.getInstance());
-		System.out.println(Chairman.getInstance());
-		System.out.println(Chairman.getInstance());
-		System.out.println(Chairman.getInstance());
-		System.out.println(Chairman.getInstance());
+		System.out.println(Chairman.getInstance("张山"));
+		System.out.println(Chairman.getInstance("李四"));
+		System.out.println(Chairman.getInstance("王五"));
+		System.out.println(Chairman.getInstance("赵六"));
+		System.out.println(Chairman.getInstance("田七"));
 	}
 
 }
--- sa/src/cn/edu/scau/cmi/liangzaoqing/singleton/Chairman.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/singleton/Chairman.java
@@ -3,16 +3,31 @@ package cn.edu.scau.cmi.liangzaoqing.singleton;
 public class Chairman {
 
 	private static Chairman chairman;
+	private String name;
 
-	private Chairman() {
+	private Chairman(String name) {
+		this.name=name;
 	}
 
-	public static Chairman getInstance() {
+	public static Chairman getInstance(String name) {
 		if (chairman == null) {
-			chairman=new Chairman();
+			chairman=new Chairman(name);
 			return chairman;
 		} else
 			return chairman;
 
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void excuteOfDuty(){
+		System.out.println("同志们辛苦了");
+	}
+	
 }
