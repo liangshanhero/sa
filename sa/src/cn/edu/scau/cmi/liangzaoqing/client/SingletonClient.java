9fa8eac0aa6f502a51c75cf8d3cec96ddf091817
xt_lzq@126.com <xt_lzq@126.com>
2018-05-08 14:56:05 +0800

    修改。

--- sa/src/cn/edu/scau/cmi/liangzaoqing/client/SingletonClient.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/SingletonClient.java
@@ -6,6 +6,25 @@ public class SingletonClient {
 
 	public static void main(String[] args) {
 
+		Chairman chairman1=Chairman.getInstance();
+		
+		
+		
+		
+		
+		
+		Chairman chairman2=Chairman.getInstance();
+		
+		if(chairman1==chairman2){
+			System.out.println("一样的");
+			
+		}
+		else
+			System.out.print("不一样的");
+			
+		
+		
+		
 		System.out.println(Chairman.getInstance());
 		System.out.println(Chairman.getInstance());
 		System.out.println(Chairman.getInstance());
