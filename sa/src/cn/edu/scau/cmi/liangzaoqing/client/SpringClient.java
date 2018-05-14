c13f263f70482d24935b5bae28781a4a8069f4f5
Liang Zaoqing <Liang Zaoqing@1230V3>
2018-05-14 18:52:26 +0800

    一个简单的Spring应用。

---
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/SpringClient.java
@@ -0,0 +1,21 @@
+package cn.edu.scau.cmi.liangzaoqing.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.edu.scau.cmi.liangzaoqing.spring.domain.Student;
+
+public class SpringClient {
+
+	public SpringClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Student student=(Student) ac.getBean("zhangsan");
+		System.out.println(student.getTutor().getName());
+
+	}
+
+}
