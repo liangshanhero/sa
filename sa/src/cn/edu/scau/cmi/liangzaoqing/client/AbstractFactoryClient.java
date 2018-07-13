626bfc4020eb44f41618a838e8ddcfcaa0071b30
Liang Zaoqing <Liang Zaoqing@1230V3>
2018-07-13 17:48:55 +0800

    修改。

--- sa/src/cn/edu/scau/cmi/liangzaoqing/client/AbstractFactoryClient.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/AbstractFactoryClient.java
@@ -6,13 +6,13 @@ import cn.edu.scau.cmi.liangzaoqing.abstractFactory.factory.AbstractFactory;
 public class AbstractFactoryClient {
 
 	public static void main(String[] args) {
-		// 获取工厂
+		// （1）获取工厂
 		
 		AbstractFactory brandProducer = AbstractFactory.getFactory("apple");
 
 		// (2)获取对象
 		Pad product = brandProducer.createPad();
-		
+//		（3）对象执行功能
 		product.play();
 
 	}
