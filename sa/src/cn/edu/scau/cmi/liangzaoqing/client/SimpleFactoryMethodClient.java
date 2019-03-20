e27eecbdf79687793f059a41390d80e86549d096
xt_lzq@126.com <liang123456>
2019-03-20 23:51:04 +0800

    rename some variable name.

--- sa/src/cn/edu/scau/cmi/liangzaoqing/client/SimpleFactoryMethodClient.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/SimpleFactoryMethodClient.java
@@ -6,8 +6,10 @@ import cn.edu.scau.cmi.liangzaoqing.simpleFactoryMethod.MeatFactory;
 public class SimpleFactoryMethodClient {
 
 	public static void main(String[] args) {
-		Meat meat = MeatFactory.getInstance("beef");
-		meat.cook();
+		
+		
+		Meat beaf = MeatFactory.getInstance("beef");
+		beaf.cook();
 		
 		Meat pork = MeatFactory.getInstance("pork");
 		pork.cook();
--- sa/src/cn/edu/scau/cmi/liangzaoqing/simpleFactoryMethod/MeatFactory.java
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/simpleFactoryMethod/MeatFactory.java
@@ -5,15 +5,14 @@ import cn.edu.scau.cmi.liangzaoqing.domain.Meat;
 import cn.edu.scau.cmi.liangzaoqing.domain.Pork;
 
 public class MeatFactory {
-	public static Meat getInstance(String meatTypeString){
-		switch(meatTypeString){
-		case "beef":return new Beef();
-		case "pork":return new Pork();
-		
+	public static Meat getInstance(String meatTypeString) {
+		Meat meat = null;
+		switch (meatTypeString) {
+		case "beef":
+			meat = new Beef();break;
+		case "pork":
+			meat = new Pork();break;
 		}
-		
-		return null;
-		
+		return meat;
 	}
-
 }
