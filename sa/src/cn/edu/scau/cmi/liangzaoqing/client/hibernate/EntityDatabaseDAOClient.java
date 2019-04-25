c8ea4fd6469809c943ec6f924ef91993d0cd4fba
xt_lzq@126.com <xt_lzq@126.com>
2019-04-25 12:11:03 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://gitee.com/liangsan/sa.git

---
+++ sa/src/cn/edu/scau/cmi/liangzaoqing/client/hibernate/EntityDatabaseDAOClient.java
@@ -0,0 +1,27 @@
+package cn.edu.scau.cmi.liangzaoqing.client.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import cn.edu.scau.cmi.liangzaoqing.hibernate.domain.Book;
+
+
+public class EntityDatabaseDAOClient {
+	public static void main(String[] args) {
+		Book book=new Book();
+		book.setName("sa");
+		book.setId(108L);
+		
+		BookDAO bookDAO=new BookDAO();
+		bookDAO.newBook(book);
+		
+		
+		bookDAO.listAllBooks();
+	}
+	
+
+
+}
