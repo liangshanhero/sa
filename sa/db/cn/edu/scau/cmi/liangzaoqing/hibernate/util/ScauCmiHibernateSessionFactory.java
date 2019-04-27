e85c100105eada1111426f3e79cfcfc4590c55ed
xt_lzq@126.com <xt_lzq@126.com>
2019-04-27 09:40:32 +0800

    modify example, move hibernate example to db source folder.

--- sa/db/cn/edu/scau/cmi/liangzaoqing/hibernate/util/ScauCmiHibernateSessionFactory.java
+++ sa/db/cn/edu/scau/cmi/liangzaoqing/hibernate/util/ScauCmiHibernateSessionFactory.java
@@ -1,12 +1,8 @@
 package cn.edu.scau.cmi.liangzaoqing.hibernate.util;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 /**
  * 简单的HibernateSessioFactory工厂类
