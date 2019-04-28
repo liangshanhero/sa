0d9fd0c4d3aa8a169c8299e37c672400c2fb9395
Administrator <Administrator@J3-605>
2019-04-28 08:50:12 +0800

    test

---
+++ sa/test/cn/edu/scau/cmi/liangzaoqing/adapter/AdapteeTest.java
@@ -0,0 +1,74 @@
+package cn.edu.scau.cmi.liangzaoqing.adapter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdapteeTest {
+
+	@Test
+	final void testOp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
