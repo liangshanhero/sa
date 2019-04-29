4e891693cafb2b1179763728c3128f272a3fd88b
xt_lzq@126.com <xt_lzq@126.com>
2019-04-29 12:05:20 +0800

    update hibernate example.

---
+++ sa/comprehension/cn/edu/scau/cmi/liangzaoqing/domain/Teacher.java
@@ -0,0 +1,37 @@
+package cn.edu.scau.cmi.liangzaoqing.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Teacher implements java.io.Serializable {
+	public Teacher() {
+	}
+
+	private Long id;
+	private String name;
+	private Set students = new HashSet(0);
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set getStudents() {
+		return this.students;
+	}
+
+	public void setStudents(Set students) {
+		this.students = students;
+	}
+}
