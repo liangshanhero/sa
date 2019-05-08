7d07742dabd5a98564f9b75870833304b27c9222
liangzaoqing <liangzaoqing@liangzaoqing-ThinkPad-X200>
2019-05-09 00:26:48 +0800

    modify spring example.

--- sa/spring/cn/edu/scau/cmi/liangzaoqing/client/SpringClient03.java
+++ sa/spring/cn/edu/scau/cmi/liangzaoqing/client/SpringClient03.java
@@ -1,35 +1,32 @@
-package cn.edu.scau.cmi.liangzaoqing.client;
+ package cn.edu.scau.cmi.liangzaoqing.client;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.edu.scau.cmi.liangzaoqing.spring.domain.Student;
+import cn.edu.scau.cmi.liangzaoqing.spring.domain.Teacher;
 import cn.edu.scau.cmi.liangzaoqing.spring.util.ApplicationContextUtil;
 
 public class SpringClient03 {
+
+	private static Teacher liangzaoqing;
+	private static Student zhangsan;
 
 	public SpringClient03() {
 	}
 
 	public static void main(String[] args) {
-		BeanFactory applicationContext = getApplicationContext1();
-		Student student1 = (Student) applicationContext.getBean("zhangsan");
-		System.out.println(student1.getName() + "的毕业设计指导导师是：" + student1.getTutor().getName());
-
+		getApplicationContext();
+		System.out.println(zhangsan.getName()+"的毕业设计指导导师是："+zhangsan.getTutor().getName());
 	}
 
-	private static ApplicationContext getApplicationContext1() {
-		ApplicationContext ac1 = new ClassPathXmlApplicationContext("springApplicationContext.xml");
+
+	private static ApplicationContext getApplicationContext() {
+		ApplicationContext applicationContext=ApplicationContextUtil.getApplicationContext();
 //		ApplicationContext ac2=new ClassPathXmlApplicationContext("applicationContext.xml");
-//		Student student1=(Student) ac1.getBean("zhangsan");
-//		Student student2=(Student) ac2.getBean("zhangsan");
-//		System.out.println(student1.getTutor().getName());
-//		System.out.println(ac1);
-//		System.out.println(ac2);
-//		System.out.println(student1);
-//		System.out.println(student2);
-		return ac1;
+		Student student1=(Student) applicationContext.getBean("zhangsan");
+
+		return applicationContext;
 	}
 
 }
