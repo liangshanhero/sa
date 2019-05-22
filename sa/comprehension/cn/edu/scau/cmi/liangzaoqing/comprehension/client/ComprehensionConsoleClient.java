6a0f17004751ab78d679caa4ce3e1b0da5b851a1
xt_lzq@126.com <xt_lzq@126.com>
2019-05-22 13:24:32 +0800

    update document.

--- sa/comprehension/cn/edu/scau/cmi/liangzaoqing/comprehension/client/ComprehensionConsoleClient.java
+++ sa/comprehension/cn/edu/scau/cmi/liangzaoqing/comprehension/client/ComprehensionConsoleClient.java
@@ -1,15 +1,11 @@
 package cn.edu.scau.cmi.liangzaoqing.comprehension.client;
-
 import org.springframework.context.ApplicationContext;
-
 import cn.edu.scau.cmi.liangzaoqing.comprehension.dao.StudentDAO;
 import cn.edu.scau.cmi.liangzaoqing.comprehension.dao.TeacherDAO;
 import cn.edu.scau.cmi.liangzaoqing.comprehension.domain.Student;
 import cn.edu.scau.cmi.liangzaoqing.comprehension.domain.Teacher;
 import cn.edu.scau.cmi.liangzaoqing.comprehension.util.ComprehensionApplicationContextUtil;
-
 public class ComprehensionConsoleClient {
-
 	public static void main(String[] args) {
 //		（1） 初始化applicationContext，并获取studentDAO对象
 		ApplicationContext applicationContext = ComprehensionApplicationContextUtil.getApplicationContext();
@@ -17,16 +13,14 @@ public class ComprehensionConsoleClient {
 		TeacherDAO teacherDAO=(TeacherDAO) applicationContext.getBean("teacherDAO");
 //		（2）创建教师和学生对象，并设置他们之间的关系
 		Teacher zhukai=new Teacher();
-		zhukai.setName("导师");
-		
+		zhukai.setName("朱凯");
 		teacherDAO.save(zhukai);
-		
 //		
-		Student wangwu=new Student();
-		wangwu.setName("学生");
+		Student lisi=new Student();
+		lisi.setName("李四");
 		
-		wangwu.setTeacher(zhukai);
+		lisi.setTeacher(zhukai);
 //		（3）保存数据
-		studentDAO.save(wangwu);
+		studentDAO.save(lisi);
 	}
 }
